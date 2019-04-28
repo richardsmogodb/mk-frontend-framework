af9dba298cabbe794a83832b3921625f2ead7030
yuji <yuji@maike51.com>
2019-04-28 17:58:34 +0800

    ll

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'mk-frontend-framework',
-    script: './node_modules/nuxt/bin/nuxt',
+    script: 'server/index.js',
     instances: 1,
     autorestart: true,
     watch: true,
