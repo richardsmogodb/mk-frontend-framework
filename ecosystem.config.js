591dbe5527961ef1dcba56d8066c0e3777b19b63
yuji <yuji@maike51.com>
2019-04-28 17:53:40 +0800

    gcvdc

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'mk-frontend-framework',
-    script: './node_modules/nuxt/bin/nuxt-start',
+    script: './node_modules/nuxt/bin/nuxt',
     instances: 1,
     autorestart: true,
     watch: true,
