8ce38d2a05ab8c23b13d190065e6771a0ca92abe
yuji <yuji@maike51.com>
2019-04-28 16:58:59 +0800

    sss

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'mk-frontend-framework',
-    script: 'server/index.js',
+    script: 'npm start',
     instances: 1,
     autorestart: true,
     watch: true,
